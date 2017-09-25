0f293b983d8e402e015f285a62c79a48e2716e12
bguzman94 <borja.guzmanlopez@gmail.com>
2017-09-25 17:12:12 +0200

    Eliminar carpeta doc y creación de tests

---
+++ src/test/java/es/upm/miw/forge/ecp2/FractionTest.java
@@ -0,0 +1,32 @@
+package es.upm.miw.forge.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class FractionTest {
+	
+	
+	private Fraction fraction;
+
+
+    @Before
+    public void before() {
+        fraction = new Fraction(1,2);
+    }
+
+    @Test
+    public void testDecimal() {
+        assertEquals(0.5, fraction.decimal(), 1);
+    }
+    
+    @Test
+    public void testGetters() {
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(2, fraction.getDenominator());
+
+    }
+    
+
+}
---
+++ src/test/java/es/upm/miw/forge/ecp2/UserTest.java
@@ -0,0 +1,34 @@
+package es.upm.miw.forge.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class UserTest {
+	
+    private User user;
+
+    @Before
+    public void before() {
+       user =  new User(1,"Borja","Guzmán");
+    }
+
+    @Test
+    public void testGetters() {
+        assertEquals(1, user.getNumber());
+        assertEquals("Borja", user.getName());
+        assertEquals("Guzmán", user.getFamilyName());
+    }
+
+    @Test
+    public void testFullName() {
+        assertEquals("Borja Guzmán", user.fullName());
+    }
+    
+    @Test
+    public void testInitials() {
+        assertEquals("B.", user.initials());
+    }
+
+}
